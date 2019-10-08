40e68b56ff8e2064798823b6da1e1bd3d611d18a
Adonay Espinosa <adonay.espinosa1@gmail.com>
2019-10-08 14:58:29 -0500

    Base App modules

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Login from "./session-components/Login";
+import Home from "./home-components/Home";
 import "./App.css";
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
-    );
-  }
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        var session = sessionStorage.getItem('session');
+        this.state = {
+            isLogged: (session) ? true : false
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <Route exact path="/" component={(this.state.isLogged) ? Home : Login} />
+                </Router>
+            </div >
+        );
+    }
 }
 
 export default App;
