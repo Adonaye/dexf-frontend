2bee49f0f6aed67cc55c98394010e254b1e90a8d
Adonay Espinosa <adonay.espinosa1@gmail.com>
2019-10-16 18:00:43 -0500

    Small changes at login

--- src/session-components/Login.js
+++ src/session-components/Login.js
@@ -5,7 +5,8 @@ class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            loginUrl: "",
+            loginUrl: "#",
+            loginError: false,
         };
 
         var config = {
@@ -18,15 +19,35 @@ class Login extends Component {
         }
         axios.get('http://127.0.0.1:3000/oauth_request', config).then(
             response => {
-                this.setState({ loginUrl: response.data.authorizeUrl });
+                this.setState({ 
+                    loginUrl: response.data.authorizeUrl,
+                });
+            },
+            error => {
+                console.log(error);
+                this.setState({ loginError: true });
             }
         );
     }
 
     render() {
         return (
-            <div>
-                <a href={this.state.loginUrl}>Link</a>
+            <div className="container">
+                <div className="row align-items-center justify-content-center" style={{height: "50vh"}}>
+                    <div className="col-6">
+                        <div className="jumbotron">
+                            <h1 className="display-4">Dexf App</h1>
+                            <p>Twitter technical assessment</p>
+                            <hr className="my-4" />
+                            <a className="btn btn-primary" role="button" href={this.state.loginUrl}>
+                                <i className="fa fa-twitter"></i> Login with Twitter
+                            </a>
+                        </div>
+                        <div className="alert alert-danger" role="alert" hidden={!this.state.loginError}>
+                            <strong>Error al conectar con el servidor.</strong> Intentelo nuevamente.
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
